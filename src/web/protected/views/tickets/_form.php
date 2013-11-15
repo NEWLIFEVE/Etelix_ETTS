28823632bc1e53870476e20fe6a275a3677c0e10
Nelson <tsu.nelsonmarcano@gmail.com>
2013-11-15 12:07:26 -0430

    Cambio de formulario de agregar tickets, agregar destination y fecha tipo arreglo, cambio de las relaciones tickets-usuarios a tickets-mail

--- src/web/protected/views/tickets/_form.php
+++ src/web/protected/views/tickets/_form.php
@@ -14,72 +14,72 @@
 	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>false,
 )); ?>
-	
+	<?php // echo 'Usuario: ' . Yii::app()->getSession()->get('usuario_id') . ' IP:' . Yii::app()->request->userHostAddress; ?>
         <fieldset>
-                <legend>Utilice este formulario para enviar un ticket.</legend>
+                <legend>Use this form to submit a ticket.</legend>
 	<?php echo $form->errorSummary($model); ?>
-                
+        
         <div class="input-control select">
-                <?php // echo $form->labelEx($model,'fallas_id'); ?>
-                <?php echo $form->dropDownList($model,'mail', CHtml::listData(Mail::getMails(Yii::app()->getSession()->get('usuario_id')), 'id', 'mail'),  array('empty' => 'Response to')); ?>
+                <?php echo $form->dropDownList($model,'mail', CHtml::listData(Mail::getMails(Yii::app()->getSession()->get('usuario_id')), 'id', 'mail'),  array('empty' => 'Response to (required)')); ?>
                 <?php echo $form->error($model,'fallas_id'); ?>
 	</div>
 
         <div class="input-control text span3">
-            <input type="text" name="tested_number[]" placeholder="Tested numbers" >
+            <?php echo $form->textField($model,'tested_numbers[]',array('placeholder' => 'Tested numbers (required)')); ?>
+            <?php echo $form->error($model,'tested_numbers[]'); ?>
+            <!--<input type="text" name="tested_number[]" placeholder="Tested numbers" >-->
         </div>
                 
         <div class="input-control select span2" style="margin-left: 5px;">
-            <?php echo $form->dropDownList($model,'mail', CHtml::listData(Destinos::model()->findAll(), 'id', 'destino'),  array('empty' => 'Destination')); ?>
-            <?php echo $form->error($model,'fallas_id'); ?>
+            <?php echo $form->dropDownList($model,'destination[]', CHtml::listData(Destinos::model()->findAll(), 'id', 'destino'),  array('empty' => 'Destination')); ?>
+            <?php echo $form->error($model,'destination[]'); ?>
         </div>
-        <div class="input-control text span2" style="margin-left: 5px;">
-            <input type="text" name="fecha[]" placeholder="Fecha" >
-        </div>
-        <div class="input-control text span1" style="margin-left: 15px; padding-top: 5px"><a type="button"><i class="icon-plus-2"></i></a></div>
                 
+        <div class="input-control text span2" style="margin-left: 5px;">
+            <?php echo $form->textField($model,'fecha[]',array('placeholder' => 'Date', 'class' => 'fecha')); ?>
+            <?php echo $form->error($model,'fecha[]'); ?>
+            <!--<input type="text" class="fecha" name="fecha[]" placeholder="Date" >-->
+        </div>
+                
+        <div class="input-control text span1" style="margin-left: 15px; padding-top: 5px"><a href="javascript:void(0)" class="_agregar"><i class="icon-plus-2"></i></a></div>
+        
+        <div class="container_agregar"></div>
       
         <div class="input-control select">
-                <?php // echo $form->labelEx($model,'fallas_id'); ?>
-                <?php echo $form->dropDownList($model,'fallas_id', CHtml::listData(Fallas::model()->findAll(), 'id', 'falla'),  array('empty' => 'Seleccione la falla')); ?>
+                <?php echo $form->dropDownList($model,'fallas_id', CHtml::listData(Fallas::model()->findAll(), 'id', 'falla'),  array('empty' => 'Failure (required)')); ?>
                 <?php echo $form->error($model,'fallas_id'); ?>
 	</div>
 
 	<div class="input-control select">
-		<?php // echo $form->labelEx($model,'prioridad_id'); ?>
-		<?php echo $form->dropDownList($model,'prioridad_id', CHtml::listData(Prioridad::model()->findAll(), 'id', 'prioridad'),  array('empty' => 'Seleccione la prioridad')); ?>
+		<?php echo $form->dropDownList($model,'prioridad_id', CHtml::listData(Prioridad::model()->findAll(), 'id', 'prioridad'),  array('empty' => 'Priority (required)')); ?>
 		<?php echo $form->error($model,'prioridad_id'); ?>
 	</div>
 
 
 	<div class="input-control text" data-role="input-control">
             <label>
-		<?php // echo $form->labelEx($model,'origen_ip'); ?>
-		<?php echo $form->textField($model,'origen_ip',array('placeholder' => 'Origination ip')); ?>
+		<?php echo $form->textField($model,'origen_ip',array('placeholder' => 'Origination ip (required)')); ?>
 		<?php echo $form->error($model,'origen_ip'); ?>
             </label>
 	</div>
 
 	<div class="input-control text" data-role="input-control">
             <label>
-		<?php // echo $form->labelEx($model,'destino_ip'); ?>
-		<?php echo $form->textField($model,'destino_ip',array('placeholder' => 'Destination ip')); ?>
+		<?php echo $form->textField($model,'destino_ip',array('placeholder' => 'Destination ip (required)')); ?>
 		<?php echo $form->error($model,'destino_ip'); ?>
             </label>
 	</div>
 
 	<div class="input-control text" data-role="input-control">
             <label>
-		<?php // echo $form->labelEx($model,'prefijo'); ?>
-		<?php echo $form->textField($model,'prefijo',array('placeholder' => 'Prefix')); ?>
+		<?php echo $form->textField($model,'prefijo',array('placeholder' => 'Prefix (required)')); ?>
 		<?php echo $form->error($model,'prefijo'); ?>
             </label>
 	</div>
 
         <div class="input-control textarea" data-role="input-control">
             <label>
-		<?php // echo $form->labelEx($model,'prefijo'); ?>
-		<?php echo $form->textArea($model,'descripcion',array('placeholder' => 'Descripción')); ?>
+		<?php echo $form->textArea($model,'descripcion',array('placeholder' => 'Description (required)')); ?>
 		<?php echo $form->error($model,'descripcion'); ?>
             </label>
 	</div>
@@ -89,8 +89,6 @@
 	
         </fieldset>
 <?php $this->endWidget(); ?>
-
-
 
 <!--<div class="grid">
 <div class="row">
