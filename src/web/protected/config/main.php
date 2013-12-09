256357cf5ceb49cff66c9e1c2dddfad8eff8eaf7
edmaku <mark182182@gmail.com>
2013-12-09 14:50:00 -0500

    bd

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -133,9 +133,9 @@ return array(
 		),
                 'soriDB'=>array(
                         'class' => 'CDbConnection',
-			'connectionString' => 'pgsql:host=172.16.17.190;port=5432;dbname=sori',
+			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=sori',
 			'username' => 'postgres',
-			'password' => '123',
+			'password' => 'Nsusfd8263',
 			'charset' => 'utf8',
 		),
 		'errorHandler'=>array(
