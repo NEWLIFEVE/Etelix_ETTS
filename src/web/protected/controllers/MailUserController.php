0ac7596fc571c4e83005e6dc02aa67c747d9366e
NEWLIFEVE <mark182182@gmail.com>
2013-12-18 13:38:51 -0430

    cambio deletemail

--- src/web/protected/controllers/MailUserController.php
+++ src/web/protected/controllers/MailUserController.php
@@ -188,6 +188,7 @@ class MailUserController extends Controller
         public function actionDeletemail()
         {   
 //            $this->loadModel($_POST['id'])->delete();
+            echo "im in";
             MailUser::model()->updateByPk($_POST['id'], array("status"=>'0'));
         }
         
