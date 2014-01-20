7e6fc4a058e137bded40e9d619d32a950836a3da
Manuel Zambrano <mmzmm3z@gmail.com>
2014-01-20 19:24:54 -0430

    Puesta en produccion v1.0.4

--- src/web/themes/metroui/views/layouts/main.php
+++ src/web/themes/metroui/views/layouts/main.php
@@ -122,7 +122,7 @@
                 </div>
             </div>
             <footer>
-                Copyright &copy; <?php echo date('Y'); ?> ETELIX All Rights Reserved. Version 1.0.3
+                Copyright &copy; <?php echo date('Y'); ?> ETELIX All Rights Reserved. Version 1.0.4
             </footer>
         </div>
         <script>
