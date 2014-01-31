cc449bb611d109b3a42a5687d2b6954f44b248fc
Manuel Zambrano <mmzmm3z@gmail.com>
2014-01-30 20:16:33 -0430

    preparando merge a master

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -119,7 +119,7 @@ return array(
                 ),
 		// uncomment the following to use a MySQL database
 		'db'=>array(
-			'connectionString'=>'pgsql:host=localhost;port=5432;dbname=dev_etts',
+			'connectionString'=>'pgsql:host=localhost;port=5432;dbname=etts',
 			'emulatePrepare'=>true,
 			'username'=>'postgres',
 			'password'=>'Nsusfd8263',
