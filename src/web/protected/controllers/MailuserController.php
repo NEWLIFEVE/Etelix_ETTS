631cd72d151a9e851b60a869206eb17b38299e48
Manuel Zambrano <mmzmm3z@gmail.com>
2014-02-17 15:35:28 -0430

    cambio de nombre

--- src/web/protected/controllers/MailuserController.php
+++ src/web/protected/controllers/MailuserController.php
@@ -1,6 +1,6 @@
 <?php
 
-class MailUserController extends Controller
+class MailuserController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
