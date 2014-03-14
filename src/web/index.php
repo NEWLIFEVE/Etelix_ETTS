4ceab1c7c2edc89681c78a1748310c186b816a06
Manuel Zambrano <mmzmm3z@gmail.com>
2014-03-14 13:05:57 -0430

    cambios

--- src/web/index.php
+++ src/web/index.php
@@ -1,20 +1,19 @@
 <?php
-/*date_default_timezone_set('America/Caracas');
+date_default_timezone_set('America/Caracas');
 //Definimos nuestro servidor de produccion
 define('SERVER_NAME_PROD','etts.etelix.com');
 //Definimos nuestro servidor de preproduccion
 define('SERVER_NAME_PRE_PROD', 'deve.sacet.com.ve');
 //Definimos nuestro servidor de desarrollo
-define('SERVER_NAME_DEV','etts.local');*/
+define('SERVER_NAME_DEV','etts.local');
 //Obtenemos el nombre del servidor actual
 $server=$_SERVER['SERVER_NAME'];
-echo $server;
 // change the following paths if necessary
-/*$yii='../../../yii/framework/yii.php';
+$yii='../../../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 switch ($server)
 {
-	case 'SERVER_NAME_PROD':
+	case SERVER_NAME_PROD:
 		defined('YII_DEBUG') or define('YII_DEBUG',false);
 		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
 		define('SERVER_DB','localhost');
@@ -22,7 +21,7 @@ switch ($server)
 		define('ETTS_DB','etts');
 		define('PASS_DB','Nsusfd8263');
 		break;
-	case 'SERVER_NAME_PRE_PROD':
+	case SERVER_NAME_PRE_PROD:
 		defined('YII_DEBUG') or define('YII_DEBUG',true);
 		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 		define('SERVER_DB','localhost');
@@ -30,7 +29,8 @@ switch ($server)
 		define('ETTS_DB','dev_etts');
 		define('PASS_DB','Nsusfd8263');
 		break;
-	case 'SERVER_NAME_DEV':
+	case SERVER_NAME_DEV:
+	default:
 		defined('YII_DEBUG') or define('YII_DEBUG',true);
 		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 		define('SERVER_DB','172.16.17.190');
@@ -41,4 +41,4 @@ switch ($server)
 }
 
 require_once($yii);
-Yii::createWebApplication($config)->run();*/
+Yii::createWebApplication($config)->run();
