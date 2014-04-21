9ba4ed0c69880f776cad3491531b51d0d999c177
edmaku <mark182182@gmail.com>
2014-04-21 15:01:09 -0430

    styles en mails

--- src/web/protected/components/Imap/Imap.php
+++ src/web/protected/components/Imap/Imap.php
@@ -119,7 +119,7 @@ class Imap extends Connection
             foreach ($idMessages as $idMessage) {
                 $body = $this->getBody($idMessage);
                 $body = $this->_filterBodyToLeft($body, $rules);
-                $body = $this->_filterBodyToRight($body, '-----');
+                $body = $this->_filterBodyToRight($body, '--Please do not reply to this email. Replies to this message are routed to an unmonitored mailbox.--');
 //                $body = $this->_filterBodyToRight($body, 'Saludos');
 //                $body = $this->_filterBodyToRight($body, 'saludos');
 //                $body = $this->_filterBodyToRight($body, 'regards');
