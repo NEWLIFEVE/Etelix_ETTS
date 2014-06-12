9dd4162fe0af1dfcf4154ac4193235eb948a6db7
Nelson <tsu.nelsonmarcano@gmail.com>
2014-06-12 10:54:36 -0430

    _subQuery() Report.php

--- src/web/protected/components/reports/Report.php
+++ src/web/protected/components/reports/Report.php
@@ -467,19 +467,18 @@ class Report extends Excel
     private function _subQuery($date, $color, $status)
     {
         $subQuery = '';
-        
         switch ($color) {
             case 'white':
                 if ($status === 'close') {
-                    $subQuery = " WHERE lifetime < '1 days'::interval AND substr(close_ticket::text, 1, 10) = '$date'";
+                    $subQuery = " WHERE lifetime < '1 days'::interval AND substr(close_ticket::text, 1, 10) = '".substr($date, 0, 10)."'";
                 } else {
-                    $subQuery = " WHERE lifetime < '1 days'::interval  AND date = '$date' AND (close_ticket IS NULL OR close_ticket > '$date')";
+                    $subQuery = " WHERE lifetime < '1 days'::interval  AND date = '".substr($date, 0, 10)."' AND (close_ticket IS NULL OR close_ticket > '$date')";
                 }
             break;
                 
             case 'yellow':
                 if ($status === 'close') {
-                    $subQuery = " WHERE lifetime >= '1 days'::interval AND lifetime < '2 days'::interval AND substr(close_ticket::text, 1, 10) = '$date'";
+                    $subQuery = " WHERE lifetime >= '1 days'::interval AND lifetime < '2 days'::interval AND substr(close_ticket::text, 1, 10) = '".substr($date, 0, 10)."'";
                 } else {
                     $subQuery = " WHERE lifetime >= '1 days'::interval AND lifetime < '2 days'::interval  AND date <= '$date' AND (close_ticket IS NULL OR close_ticket > '$date')";
                 }
@@ -487,7 +486,7 @@ class Report extends Excel
             
             case 'red':
                 if ($status === 'close') {
-                    $subQuery = " WHERE lifetime >= '2 days'::interval AND substr(close_ticket::text, 1, 10) = '$date'";
+                    $subQuery = " WHERE lifetime >= '2 days'::interval AND substr(close_ticket::text, 1, 10) = '".substr($date, 0, 10)."'";
                 } else {
                     $subQuery = " WHERE lifetime >= '2 days'::interval AND date <= '$date' AND (close_ticket IS NULL OR close_ticket > '$date')";
                 }
