e2761a42fbadb2820b54f7efbffd8bbd41ba23c0
Manuel Zambrano <mmzmm3z@gmail.com>
2014-07-09 15:16:17 -0430

    Release 1.3.2

--- src/web/themes/metroui/views/layouts/main.php
+++ src/web/themes/metroui/views/layouts/main.php
@@ -108,7 +108,7 @@
                 </div>
             </div>
             <footer>
-                Copyright &copy; <?php echo date('Y'); ?> ETELIX All Rights Reserved. Version 1.3.1
+                Copyright &copy; <?php echo date('Y'); ?> ETELIX All Rights Reserved. Version 1.3.2
             </footer>
         </div>
         <script>
